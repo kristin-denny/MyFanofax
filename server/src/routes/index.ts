a506b8e73a46a0d4fe4399439c66c8717b6852fa
kristin-denny <kristingdenny@gmail.com>
2024-11-20 00:04:53 -0500

    fixed auth route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 import { userRouter } from './user-routes.js';
 
 const router = Router();
 
-router.use('/auth', authRoutes, userRouter);
+router.use('/auth', userRouter);
 router.use('/api', authenticateToken,  apiRoutes);
 
 export default router;
